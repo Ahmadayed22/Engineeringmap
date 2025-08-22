905150cedeeb580be931f26ba373efc6314067b4
Ahmadayed22 <ahmad.ayed461@gmail.com>
2025-08-22 13:38:19 +0300

    try another solution

--- server/src/main/java/com/engineeringmap/server/controller/SPAController.java
+++ server/src/main/java/com/engineeringmap/server/controller/SPAController.java
@@ -4,16 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @Controller
 public class SPAController {
-  // Matches root "/"
-    @GetMapping("/")
-    public String root() {
-        return "forward:/index.html";
-    }
-
-    // Matches all frontend routes except API routes
-    @GetMapping("/{path:^(?!api$).*$}/**")
+ @GetMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
